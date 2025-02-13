96d7d51f06ebae407bba8b023c72c2eaa4385975
Silajeet0 <silajeetbanerjee100@gmail.com>
2025-02-13 10:37:57 +0530

    Added .gitignore to ignore Maven build files

---
+++ src/main/java/com/jSend/emailclient/App.java
@@ -0,0 +1,13 @@
+package com.jSend.emailclient;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
